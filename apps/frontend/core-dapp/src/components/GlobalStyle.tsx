dbe04119dd90ae8683a9d8c891f2458bf3eca6e9
Pablo Rodríguez <pablo@rodriguezcaballero.me>
2023-02-09 14:20:00 +0100

    chore: remove unnecessary `font-size` prop (#859)

--- apps/frontend/core-dapp/src/components/GlobalStyle.tsx
+++ apps/frontend/core-dapp/src/components/GlobalStyle.tsx
@@ -29,7 +29,6 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     border: 0;
-    font-size: 100%;
     font: inherit;
     vertical-align: baseline;
   }
--- apps/frontend/react-boilerplate/src/components/GlobalStyle.tsx
+++ apps/frontend/react-boilerplate/src/components/GlobalStyle.tsx
@@ -25,7 +25,6 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     border: 0;
-    font-size: 100%;
     font: inherit;
     vertical-align: baseline;
   }
