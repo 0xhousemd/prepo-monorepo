9ab11fee054c1904f85eac1c50ffa2ce63b1a69b
Chris Ling <81092286+chrisling-dev@users.noreply.github.com>
2023-02-10 14:04:19 +0800

    feat: withdraw button (#819)
    * feat: withdraw input
    
    * fix: remove unused withdraw input file
    
    * feat: withdraw input
    
    * feat: withdraw button
    
    * fix: withdraw store tests
    
    * fix: only reset input if no error
    
    * feat: address comments
    
    * fix: toast message

--- apps/frontend/core-dapp/src/features/withdraw/WithdrawStore.ts
+++ apps/frontend/core-dapp/src/features/withdraw/WithdrawStore.ts
@@ -1,9 +1,10 @@
 import { BigNumber } from 'ethers'
-import { makeAutoObservable } from 'mobx'
+import { makeAutoObservable, runInAction } from 'mobx'
 import { validateStringToBN } from 'prepo-utils'
 import { RootStore } from '../../stores/RootStore'
 
 export class WithdrawStore {
+  withdrawing = false
   withdrawalAmount = ''
 
   constructor(public root: RootStore) {
@@ -14,15 +15,36 @@ export class WithdrawStore {
     if (validateStringToBN(amount)) this.withdrawalAmount = amount
   }
 
-  // eslint-disable-next-line require-await
-  async withdraw(): Promise<{
-    success: boolean
-    error?: string | undefined
-  }> {
-    const { preCTTokenStore } = this.root
-    return this.withdrawalAmountBN !== undefined && this.withdrawalAmountBN.gt(0)
-      ? preCTTokenStore.withdraw(this.withdrawalAmountBN)
-      : { success: false }
+  async withdraw(): Promise<void> {
+    if (
+      this.insufficientBalance ||
+      this.withdrawalAmountBN === undefined ||
+      this.withdrawalAmountBN.eq(0)
+    )
+      return
+
+    this.withdrawing = true
+    const { error } = await this.root.preCTTokenStore.withdraw(this.withdrawalAmountBN)
+
+    if (error) {
+      this.root.toastStore.errorToast('Withdrawal failed', error)
+    } else {
+      this.root.toastStore.successToast('Withdrawal successful 🎉')
+    }
+
+    runInAction(() => {
+      this.withdrawing = false
+      if (!error) this.withdrawalAmount = ''
+    })
+  }
+
+  get insufficientBalance(): boolean | undefined {
+    if (
+      this.withdrawalAmountBN === undefined ||
+      this.root.preCTTokenStore.balanceOfSigner === undefined
+    )
+      return undefined
+    return this.withdrawalAmountBN.gt(this.root.preCTTokenStore.balanceOfSigner)
   }
 
   get isLoadingBalance(): boolean {
@@ -35,12 +57,13 @@ export class WithdrawStore {
   }
 
   get withdrawalDisabled(): boolean {
-    const { tokenBalanceRaw } = this.root.preCTTokenStore
     return (
-      tokenBalanceRaw === undefined ||
+      this.receivedAmountBN === undefined ||
       !this.withdrawalAmountBN ||
       this.withdrawalAmountBN.lte(0) ||
-      this.withdrawalAmountBN.gt(tokenBalanceRaw)
+      this.insufficientBalance === undefined ||
+      this.insufficientBalance ||
+      this.withdrawUILoading
     )
   }
 
@@ -71,18 +94,17 @@ export class WithdrawStore {
   }
 
   get withdrawUILoading(): boolean {
-    const { withdrawing } = this.root.preCTTokenStore
-    return (
-      withdrawing ||
-      this.receivedAmountBN === undefined ||
-      this.withdrawalAmountBN === undefined ||
-      this.isLoadingBalance
-    )
+    return this.withdrawing || this.withdrawButtonInitialLoading
   }
 
   get withdrawalFee(): number | undefined {
     const { redemptionFee, feeDenominator } = this.root.preCTTokenStore
     if (redemptionFee === undefined || feeDenominator === undefined) return undefined
     return redemptionFee.toNumber() / feeDenominator.toNumber()
+  }
+
+  get withdrawButtonInitialLoading(): boolean {
+    if (this.withdrawalAmount === '') return false
+    return Boolean(this.isLoadingBalance || this.insufficientBalance === undefined)
   }
 }
