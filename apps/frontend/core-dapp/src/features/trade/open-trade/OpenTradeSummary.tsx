2fbacf68af9c1d0931e2bbc4d9b1bc5034d72216
Chris Ling <81092286+chrisling-dev@users.noreply.github.com>
2023-02-22 10:58:51 +0800

    feat: open trade page show unprofitable when out of bound (#913)

--- apps/frontend/core-dapp/src/features/trade/open-trade/OpenTradeSummary.tsx
+++ apps/frontend/core-dapp/src/features/trade/open-trade/OpenTradeSummary.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite'
 import { Flex } from 'prepo-ui'
+import styled from 'styled-components'
 import Skeleton from '../../../components/Skeleton'
 import SummaryRecord from '../../../components/SummaryRecord'
 import { useRootStore } from '../../../context/RootStoreProvider'
@@ -7,28 +8,29 @@ import { numberFormatter } from '../../../utils/numberFormatter'
 import { EstimatedValuation } from '../../definitions'
 
 const { significantDigits } = numberFormatter
+
+const RedText = styled.span`
+  color: ${({ theme }): string => theme.color.error};
+`
+
 const OpenTradeSummary: React.FC = () => {
   const { tradeStore } = useRootStore()
   const { withinBounds, tradingValuation, selectedMarket, openTradeAmountBN } = tradeStore
 
-  if (
-    !selectedMarket ||
-    openTradeAmountBN === undefined ||
-    openTradeAmountBN.eq(0) ||
-    withinBounds === false
-  )
-    return null
+  if (!selectedMarket || openTradeAmountBN === undefined || openTradeAmountBN.eq(0)) return null
 
   const loading = tradingValuation === undefined || withinBounds === undefined
 
+  const renderValueUI = (): React.ReactNode => {
+    if (loading) return <Skeleton height="22px" width="64px" />
+    if (!withinBounds) return <RedText>Unprofitable</RedText>
+    return `$${significantDigits(tradingValuation)}`
+  }
+
   return (
     <Flex width="100%" px={12} pb={8}>
-      <SummaryRecord label="Estimated Valuation Price" tooltip={<EstimatedValuation />}>
-        {loading ? (
-          <Skeleton height="22px" width="64px" />
-        ) : (
-          `$${significantDigits(tradingValuation)}`
-        )}
+      <SummaryRecord label="Estimated Price" tooltip={<EstimatedValuation />}>
+        {renderValueUI()}
       </SummaryRecord>
     </Flex>
   )
